1042685a955d327ea0ee03405f18b757ceca985e
Prashant singh <prashant702200@gmail.com>
2026-03-19 14:28:39 +0530

    first commit

---
+++ operator/index.js
@@ -0,0 +1,19 @@
+//operator arithmatic
+let a=38;
+let b=48;
+console.log(a**b); //unary operator 
+// pre increment ++a pahle increse kero fir print kero 
+// post increment pahle a-- pahle print kero fir plus ker ke print kero same for --//
+let u =10;
+u = u+10;
+console.log(u);
+//assigment operator//
+console.log( a<=b);
+console.log(a>b); // == lose equality === strict equality //  
+//ternory opertor 
+let age =12;
+let pus1 = (age>18) ? 'i can vote': 'i cannot vote';
+console.log(pus1)
+
+
+
