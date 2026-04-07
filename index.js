a0cdbea7fc22d7a00fe673d1ab85eb2692fe729a
Prashant singh <prashant702200@gmail.com>
2026-04-07 22:04:15 +0530

    changes

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("hello code babber")
+function pus(){
+    var age =69;
+    console.log(age);
+}
+pus();
+{
+    let age =69;
+    console.log(age);
+}
+let a=10;
+a="code babber";
+let marks = 2**4;
+console.log(marks);
